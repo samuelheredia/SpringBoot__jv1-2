de0ff56d9af598ebf1560aa9123328bc06c2b1b6
Samuel Heredia <samuel.heredia@bosonit.com>
2021-10-20 10:56:53 +0200

    Initial commit

---
+++ src/main/java/com/bosonit/springboot/jv12/Jv12Application.java
@@ -0,0 +1,13 @@
+package com.bosonit.springboot.jv12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jv12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jv12Application.class, args);
+	}
+
+}
