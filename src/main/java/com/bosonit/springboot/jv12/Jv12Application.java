1d4f627f9ab74274b6ea7f3ad0acefe61e400ae5
Samuel Heredia <samuel.heredia@bosonit.com>
2021-10-20 11:03:13 +0200

    JV1 y JV2

--- src/main/java/com/bosonit/springboot/jv12/Jv12Application.java
+++ src/main/java/com/bosonit/springboot/jv12/Jv12Application.java
@@ -1,13 +1,45 @@
 package com.bosonit.springboot.jv12;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootApplication
 public class Jv12Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Jv12Application.class, args);
+	public static void main(String[] args){
+		File archive;
+		FileReader fr = null;
+		BufferedReader br;
+		List<Persona> personas = new ArrayList<>();
+
+		try {
+			// Lectura corriente de fichero
+			archive = new File ("fich.txt");
+			fr = new FileReader (archive);
+			br = new BufferedReader(fr);
+			String linea;
+			while( (linea = br.readLine() ) != null)
+				personas.add( new Persona(linea));
+
+			// Localiza mediante stream menores 25 (edad null = 26)
+			personas.parallelStream()
+					.filter(p -> p.getEdad().orElse(99) < 25)
+					.forEach( p -> System.out.println( p ));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if( null != fr ){
+					fr.close();
+				}
+			}catch (Exception e2){
+				e2.printStackTrace();
+			}
+		}
+
 	}
 
 }
